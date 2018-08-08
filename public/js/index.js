a3d91f51c7f428ef750a2c203e5fc1684a51a4a7
Max Visser <maxvisser@gmail.com>
2018-08-08 13:52:51 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ if(location.hostname == "rocketchat.github.io" && location.href.indexOf('?noredi
 
 function scroll_toc(path) {
   // remove base either '/docs/' or '/'
-  var base = '/docs/';
+  var base = '/en/';
 
   path = path.indexOf(base) == 0? path.substring(base.length) : path.substring(1);
 
